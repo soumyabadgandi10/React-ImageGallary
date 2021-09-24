9c483c9222c5b1394a8925f95028218da21ca65e
soumyabadgandi10 <soumyabadgandi10@users.noreply.github.com>
2021-09-24 19:10:29 +0530

    add content

---
+++ src/Helper/CarouselData.js
@@ -0,0 +1,11 @@
+import img1 from "../Images/img1.jfif"
+import img2 from "../Images/img2.jfif"
+import img3 from "../Images/img3.jfif"
+import img4 from "../Images/img4.jfif"
+
+export const images = [
+    {title: "Ladakh", subtitle: "text", img: img1},
+    {title: "Shimla", subtitle: "text", img: img2},
+    {title: "Manali", subtitle: "text", img: img3},
+    {title: "Kashmir", subtitle: "text", img: img4}
+];
